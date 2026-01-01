7cf0b879dcaa1f82e6a81660b2427ecad53561e0
Elliott Porter <elliott@weblake.co.uk>
2026-01-01 20:25:39 +0000

    commit changes

---
+++ Quiz/src/app/QuizScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const QuizScreen = () => {
+  return (
+    <View>
+      <Text>Quiz screen</Text>
+    </View>
+  );
+};
+
+export default QuizScreen;
