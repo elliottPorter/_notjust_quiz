cec3450c6659f48ccfd90344f914af96cd4d2c29
Elliott Porter <elliott@weblake.co.uk>
2026-01-06 19:56:26 +0000

    First QuizScreen.tsx

--- Quiz/src/app/QuizScreen.tsx
+++ Quiz/src/app/QuizScreen.tsx
@@ -2,8 +2,13 @@ import { View, Text } from 'react-native';
 
 const QuizScreen = () => {
   return (
-    <View>
-      <Text>Quiz screen</Text>
+    <View style={{
+      backgroundColor:'#fdfef4',
+      flex:1,
+      padding:20,
+      justifyContent:'center'
+    }}>
+      <Text>Chester's Quiz screen</Text>
     </View>
   );
 };
